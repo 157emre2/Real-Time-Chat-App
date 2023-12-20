98dfbcb39619b99cae1dfbad6d6ea0b090aa30b9
157emre2 <157emre2@gmail.com>
2023-12-20 15:26:33 +0300

    .

--- server/models/user.js
+++ server/models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-   name: String,
-   username: String,
-   password: String,
-   email: String,
-   chatId: [String]
+   name: {type: String, required: true},
+   username: {type: String, required: true},
+   password: {type: String, required: true},
+   email: {type: String, required: true},
+   chatId: {type: [String], required: false}
 });
 
 module.exports = mongoose.model('User',UserSchema);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,15 @@
-
 const graphql = require('graphql');
-const { GraphQLList } = require("graphql/type");
 const Message = require('../models/message');
 const User = require('../models/user');
 const Chat = require('../models/chat');
+
 const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
+    GraphQLList,
+    GraphQLSchema,
+    GraphQLNonNull
 } = graphql;
 
 
@@ -75,7 +77,6 @@ const ChatType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        // Chats, Messages in chat, Users in chat, Message, User, Search (User, Message, Chat)
         chats: {
             type: new GraphQLList(ChatType),
             args: { userId: { type: GraphQLID}},
@@ -103,6 +104,78 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,args){
                 return Message.findById(args.id);
             }
+        },
+        messages: {
+            type: new GraphQLList(MessageType),
+            resolve(parent,args){
+                return Message.find();
+            }
+        },
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLID}},
+            resolve(parent,args){
+                return User.findById(args.id);
+            }
+        },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent,args){
+                return User.find();
+            }
         }
+        //Search Queries will add
     }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        //Add-Update-Delete User Mutations
+        addUser: {
+            type: UserType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                username: { type: new GraphQLNonNull(GraphQLString)},
+                password: { type: new GraphQLNonNull(GraphQLString)},
+                email: { type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parent,args){
+                 let user = new User({
+                   name: args.name,
+                   username: args.username,
+                   password: args.password,
+                   email: args.email
+                });
+                return user.save();
+            }
+        },
+        updateUser: {
+            type: UserType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLID)},
+                name: { type: GraphQLString },
+                username: { type: GraphQLString },
+                password: { type: GraphQLString },
+                email: { type: GraphQLString }
+            },
+            resolve(parent, args){
+                return User.findByIdAndUpdate(args.id, {
+                    name: args.name,
+                    username: args.username,
+                    password: args.password,
+                    email: args.email
+                }, {new: true});
+            }
+        }
+        //Add-Update-Delete Chat Mutations
+        //Add-Update-Delete Message Mutations
+
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
 });
